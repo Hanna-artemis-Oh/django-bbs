bd24d4920c350ed442607f3b069482c01f82b960
Hanna-artemis-Oh <artemiss02@hanmail.net>
2023-08-07 14:51:51 +0900

    separate features of function into two ways by request methods

--- bbs/views.py
+++ bbs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Board
+from .forms import BoardForm
 
 
 def b_list(request):
@@ -10,4 +11,10 @@ def b_list(request):
 
 
 def b_create(request):
-    pass
+    if request.method == 'POST':
+        # post 방식 호출: 데이터베이스에 저장
+        pass
+    else:
+        # get 방식 호출: 사용자 입력 양식 화면 출력
+        board_form = BoardForm()
+    return render(request, 'bbs/create.html', {'board_form': board_form})
