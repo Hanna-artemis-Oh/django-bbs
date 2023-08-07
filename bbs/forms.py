87a3fa7e8b228644d30fb32590f67af75864be50
Hanna-artemis-Oh <artemiss02@hanmail.net>
2023-08-07 16:16:50 +0900

    make model form and apply to html file

--- bbs/forms.py
+++ bbs/forms.py
@@ -6,3 +6,30 @@ class BoardForm(forms.ModelForm):
     class Meta:
         model = Board
         fields = ['b_title', 'b_author', 'b_content']
+
+        labels = {
+            'b_title': '글 제목',
+            'b_author': '글 작성자',
+            'b_content': '글 내용'
+        }
+
+        widgets = {
+            'b_title': forms.TextInput(
+                attrs={
+                    'class': 'form-control w-50 mb-3',
+                    'placeholder': '글 제목을 입력하세요',
+                }
+            ),
+            'b_author': forms.TextInput(
+                attrs={
+                    'class': 'form-control w-25 mb-3',
+                    'placeholder': '글 작성자를 입력하세요'
+                }
+            ),
+            'b_content': forms.Textarea(
+                attrs={
+                    'class': 'form-control w-75 mb-3',
+                    'placeholder': '글 내용을 입력하세요'
+                }
+            )
+        }
--- bbs/views.py
+++ bbs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board
 from .forms import BoardForm
 
@@ -13,7 +13,14 @@ def b_list(request):
 def b_create(request):
     if request.method == 'POST':
         # post 방식 호출: 데이터베이스에 저장
-        pass
+        # 사용자가 입력한 내용 포함
+        board_form = BoardForm(request.POST)
+
+        if board_form.is_valid():
+            new_post = board_form.save(commit=False)
+            new_post.b_like_count = 100
+            new_post.save()
+            return redirect('bbs:b_list')
     else:
         # get 방식 호출: 사용자 입력 양식 화면 출력
         board_form = BoardForm()
