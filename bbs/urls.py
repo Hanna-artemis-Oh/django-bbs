322523f95ab07e6498929cb0f77044400d5e8936
Hanna-artemis-Oh <artemiss02@hanmail.net>
2023-08-07 12:22:41 +0900

    add list page

--- bbs/urls.py
+++ bbs/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
+
+app_name = 'bbs'  # namespace 설정
 
 urlpatterns = [
-    # path(""),
+    path("list/", views.b_list, name='b_list'),
 ]
--- bbs/views.py
+++ bbs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Board
 
-# Create your views here.
+
+def b_list(request):
+    # 게시글을 모두 가져와서 화면에 출력
+    posts = Board.objects.all().order_by('-id')
+    context = {'posts': posts}
+    return render(request, 'bbs/list.html', context)
